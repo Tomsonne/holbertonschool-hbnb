bf78f353fd6164efbfd1e37a49178b1e1ce05e3b
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-06-16 14:36:41 +0200

    ajout des classes place et review

---
+++ part2/app/models/review.py
@@ -0,0 +1,10 @@
+from models import BaseModel
+
+
+class review(BaseModel):
+    def __init__(self, text, rating, place, user):
+        super().__init__()
+        self.text = text
+        self.rating = rating
+        self.place = place
+        self.user = user
