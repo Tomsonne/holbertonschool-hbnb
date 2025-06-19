f70140d8a77ddd3837d068a2fffbd34926b24ec2
Tomsonne <thomas.rousseau19@gmail.com>
2025-06-19 17:34:55 +0200

    suppression de newsier = .. users.py creation de 2user

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -28,8 +28,6 @@ class UserList(Resource):
             new_user = facade.create_user(user_data)
         except ValueError as e:
             return {'error': str(e)}, 400
-            
-        new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
     
     @api.response(200, 'Users retrieved successfully')
